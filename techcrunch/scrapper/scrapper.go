0d24fada7f6a9c71532d40bb7cf7cc0960c24096
Rodrigo Andrade <raeros42@gmail.com>
2021-04-20 23:17:14 -0300

    Starting changing script to get tags from body.w

--- techcrunch/scrapper/scrapper.go
+++ techcrunch/scrapper/scrapper.go
@@ -1,22 +1,41 @@
 package scrapper                                                                                                                                                                          
 import (
   "golang.org/x/net/html"
-  "io")
+  "io"
+)
 
-func isTitleElement(n *html.Node) bool {                                                        return n.Type == html.ElementNode && n.Data == "title"                                    
-}                                                                                             
-func traverse(n *html.Node) (string, bool) {                                                
-	  if isTitleElement(n) {                                                               
-  	  return n.FirstChild.Data, true                                                        
-	  }                                                                                           
-    for c := n.FirstChild; c != nil; c = c.NextSibling {                                     
-      result, ok := traverse(c)                                                                
-      if ok {                                                                               
-        return result, ok                                                                    
-      }                                                                                      
-    }                                                                                           
-    return "", false                                                                            
-}                                                                                             
+func isTitleElement(n *html.Node) bool {
+  return n.Type == html.ElementNode && n.Data == "body"
+  //return n.Type == html.ElementNode && n.Data == "title"
+}
+
+func traverse(n *html.Node) (string, bool) {
+
+	  if isTitleElement(n) {
+       println("___")
+       println(n.LastChild.Namespace)
+       println("___")
+       println(n.LastChild.Data)
+       println("___")
+       println(n.LastChild.Type)
+       println("___")
+       println(n.LastChild.DataAtom)
+       println("___")
+       println(n.LastChild.Attr)
+       println("___")
+
+      return n.FirstChild.Data, true
+	  }       
+
+    for c := n.FirstChild; c != nil; c = c.NextSibling {                                  
+      result, ok := traverse(c)
+      if ok {
+        return result, ok
+      }
+    }
+
+    return "", false
+}                                                                                          
                                                                                               
 func GetHtmlTitle(body io.Reader) (string, bool) {                                            
     body_parsed, err := html.Parse(body)
